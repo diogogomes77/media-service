c807b8d87ed9cf2466bf038a48a4c9c2c4a8e2d7
Diogo Gomes <diogo.gomes77@gmail.com>
2022-05-07 15:16:31 +0100

    feat(5): convert to typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import http from 'http';
+
+// const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! It is onverted from typescript');
+});
+
+server.listen(port, () => {
+  //console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at port ${port}/`);
+});
